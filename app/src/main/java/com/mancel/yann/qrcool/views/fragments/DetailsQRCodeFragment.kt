c017564bde469e550148bb9b5ca3fe7ac8d8a15d
Yann Mancel <yann.mancel@hotmail.fr>
2020-08-10 14:50:23 +0200

    Add Button in DetailsQRCodeFragment.kt

--- app/src/main/java/com/mancel/yann/qrcool/views/fragments/DetailsQRCodeFragment.kt
+++ app/src/main/java/com/mancel/yann/qrcool/views/fragments/DetailsQRCodeFragment.kt
@@ -1,5 +1,9 @@
 package com.mancel.yann.qrcool.views.fragments
 
+import android.content.Intent
+import android.net.Uri
+import android.view.View
+import android.webkit.URLUtil
 import com.mancel.yann.qrcool.R
 import kotlinx.android.synthetic.main.fragment_details_q_r_code.view.*
 
@@ -32,7 +36,37 @@ class DetailsQRCodeFragment : BaseFragment() {
      * Updates the UI
      */
     private fun updateUI() {
+        val rawValue = this._qRCode._text
+
         // Raw value
-        this._rootView.fragment_details_raw_value.text = this._qRCode._text
+        this._rootView.fragment_details_raw_value.text = rawValue
+
+        // Button according to if raw value is a valid url
+        val visibility = if (URLUtil.isValidUrl(rawValue)) {
+            // Listener of Button
+            this._rootView.fragment_details_button.setOnClickListener {
+                this.openLinkFromUrl(rawValue)
+            }
+
+            View.VISIBLE
+        } else {
+            View.GONE
+        }
+
+        this._rootView.fragment_details_button.visibility = visibility
+    }
+
+    // -- URL --
+
+    /**
+     * Opens a link thanks to the URL in argument
+     * @param url a [String] that contains the URL
+     */
+    private fun openLinkFromUrl(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse(url)
+        }
+
+        this.startActivity(intent)
     }
 }
